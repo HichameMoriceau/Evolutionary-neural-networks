4fc5834c1bff1b24b598a120656d8608c0a4b7d1
HichameMoriceau <moriceau.hichame@gmail.com>
2016-05-26 18:17:31 +0100

    Added script to generate 'perfs VS nb of calls to error function'.

--- evolutionary_nets/build-evolutionary_nets-Desktop-Debug/plotter.m
+++ evolutionary_nets/build-evolutionary_nets-Desktop-Debug/plotter.m
@@ -15,8 +15,8 @@ cd 'images/';
 population_size	= results(1,9);
 nb_replicates   = results(1,end);
 
-AVRG_CV_FITNESS = results(1,end-3);
-AVRG_CV_ACC = results(1,end-2);
+TEST_SCORE = results(1,end-3);
+TEST_ACC = results(1,end-2);
 
 
 #
@@ -26,36 +26,55 @@ align_right = results(end,1) - (results(end,1)/3) - 26;
 errorbar(results(:,1), results(:,4), err_results);
 hold on;
 grid on;
-plot(results(:,1),results(:,3),  'k', 'LineWidth', 1);
+plot(results(:,1),results(:,16), 'm', 'LineWidth', 1);
+plot(results(:,1),results(:,18), 'g', 'LineWidth', 1, 'linestyle', '--');
 plot(results(:,1),results(:,4),  'r', 'LineWidth', 1);
-plot(results(:,1),results(:,17), 'm', 'LineWidth', 1);
+plot(results(:,1),results(:,3),  'k', 'LineWidth', 1);
 axis([0, results(end,1), 0, 100]);
-title( strcat(data_set_name, " - population size : ", num2str(population_size), "[", num2str(nb_replicates), " replicates]"));
-legend('[Error amongst replicates] Corrected sample standard deviation','Prediction accuracy','F1 score of the best individual', 'F1 score of the ensemble', "location", "southeast");
+title( strcat(data_set_name, " - population size : ", num2str(population_size), " - ", num2str(nb_replicates), " replicates"));
+legend('[Error amongst replicates] Corrected sample standard deviation','Prediction accuracy','Best indiv score on training set', 'Ensemble accuracy', 'Best indiv accuracy on CV set', "location", "southeast");
 xlabel('Number of generations');
 ylabel('Performance of best individual on each cross-validation set');
-text(align_right, 46 , strcat("Average score on CV sets =", num2str(AVRG_CV_FITNESS)));
-text(align_right, 42 , strcat("Average acc on CV sets =", num2str(AVRG_CV_ACC)));
+text(align_right, 50 , strcat("Score on unseen test data =", num2str(TEST_SCORE)));
+text(align_right, 46 , strcat("Score on unseen test data =", num2str(TEST_ACC)));
 # add topology description
-text(align_right, 38 , "Final topology:");
-text(align_right, 34 , strcat("NB inputs            =", num2str(results(end,10))) );
-text(align_right, 30 , strcat("NB hidden units  =", num2str(results(end,11))) );
-text(align_right, 26 , strcat("NB outputs          =", num2str(results(end,12))) );
-text(align_right, 22 , strcat("NB hidden layers=", num2str(results(end,13))) );
-
-plot_name = strcat(data_set_name, "-performancesVSepochs.png");
+text(align_right, 42 , "Final topology:");
+text(align_right, 38 , strcat("NB inputs            =", num2str(results(end,10))) );
+text(align_right, 34 , strcat("NB hidden units  =", num2str(results(end,11))) );
+text(align_right, 30 , strcat("NB outputs          =", num2str(results(end,12))) );
+text(align_right, 26 , strcat("NB hidden layers=", num2str(results(end,13))) );
+plot_name = strcat(data_set_name, "-perfsVSepochs.png");
 print('-dpng', '-tiff', plot_name)
 hold off;
 
-plot(results(:,1),results(:,5), 'b', 'LineWidth', 1);
+#
+# NB OF CALLS TO ERROR FUNCTION
+#
+align_right = results(end,1) - (results(end,1)/3) - 26;
+errorbar(results(:,20), results(:,4), err_results);
+plot(results(:,20),results(:,16), 'm');
 hold on;
 grid on;
-legend('Mean Squared Error', "location", "northeast");
-xlabel('Number of generations');
-ylabel('MSE of best individual on each cross-validation set');
-plot_name = strcat(data_set_name, "-MSEVSepochs.png");
-print('-dpng', '-tiff', plot_name);
+plot(results(:,20),results(:,16), 'm', 'LineWidth', 1);
+plot(results(:,20),results(:,3),  'r', 'LineWidth', 1);
+plot(results(:,20),results(:,4),  'k', 'LineWidth', 1);
+plot(results(:,20),results(:,18), 'g', 'LineWidth', 1, 'linestyle', '--');
+title( strcat(data_set_name, " - population size : ", num2str(population_size), " - ", num2str(nb_replicates), " replicates"));
+legend('[Error amongst replicates] Corrected sample standard deviation', 'Ensemble accuracy', 'Best indiv accuracy on training set','Best indiv f1 Score on training set', 'Best indiv accuracy on CV set', "location", "southeast");
+xlabel('Number of calls to the error function');
+ylabel('Performance of best individual, ensemble set');
+text(align_right, 50 , strcat("Score on unseen test data =", num2str(TEST_SCORE)));
+text(align_right, 46 , strcat("Score on unseen test data =", num2str(TEST_ACC)));
+# add topology description
+text(align_right, 42 , "Final topology:");
+text(align_right, 38 , strcat("NB inputs            =", num2str(results(end,10))) );
+text(align_right, 34 , strcat("NB hidden units  =", num2str(results(end,11))) );
+text(align_right, 30 , strcat("NB outputs          =", num2str(results(end,12))) );
+text(align_right, 26 , strcat("NB hidden layers=", num2str(results(end,13))) );
+plot_name = strcat(data_set_name, "-perfsVSnbcallstoerrorfunction.png");
+print('-dpng', '-tiff', plot_name)
 hold off;
+
 
 #
 # POPULATION SCORE STATS
