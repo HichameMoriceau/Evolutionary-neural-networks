82dc5b9a7c0321ba4b6257603e9d8fe9dbdc76a5
HichameMoriceau <moriceau.hichame@gmail.com>
2016-06-01 17:28:10 +0100

    Scaled up current BP experiment to all other data sets.

--- evolutionary_nets/build-evolutionary_nets-Desktop-Debug/BP_experiment/main.cpp
+++ evolutionary_nets/build-evolutionary_nets-Desktop-Debug/BP_experiment/main.cpp
@@ -1,37 +1,76 @@
 #include<iostream>
-#include <fann.h>
-#include <vector>
+#include<string>
+#include<vector>
+#include<fann.h>
+#include<armadillo>
 using namespace std;
+using namespace arma;
 
-int main(){  
+vector<string> split(const string& str, int delimiter(int) = ::isspace){
+  vector<string> result;
+  auto e=str.end();
+  auto i=str.begin();
+  while(i!=e){
+    i=find_if_not(i,e, delimiter);
+    if(i==e) break;
+    auto j=find_if(i,e, delimiter);
+    result.push_back(string(i,j));
+    i=j;
+  }
+  return result;
+}
 
+/**
+   Experiment: Running multiple instances of a neural network
+   trained using Gradient Descent and the Back Propagation algorithm.
+   Author: Hichame Moriceau
+   Compile & Run with: 
+   # g++ -std=c++11 main.cpp -larmadillo -lfann -o runme
+   # ./runme
+*/
+int main(){
+  //const char* ds_filename="data/breast-cancer-recurrence-data-transformed.data";//"data_sets/breast-cancer-malignantOrBenign-data-transformed.data";
+  unsigned int nb_opt_algs=5;
+  
+  vector<char*> ds_filenames;
+  ds_filenames.push_back((char*)"data/wine-data-transformed.data"); // multi-class
+  ds_filenames.push_back((char*)"data/breast-cancer-recurrence-data-transformed.data");
+  ds_filenames.push_back((char*)"data/iris-data-transformed.data"); // multi-class
+  ds_filenames.push_back((char*)"data/breast-cancer-malignantOrBenign-data-transformed.data");
 
-  const char* ds_filename="data_sets/breast-cancer-recurrence-data-transformed.data";//"data_sets/breast-cancer-malignantOrBenign-data-transformed.data";
-  unsigned int nb_opt_algs=5;  
+  // selected data set
+  string ds_filename=string(ds_filenames[0]);
+  
+  // read data set header
+  ifstream in(ds_filename);
+  if(!in.is_open()){
+    cout<<"Incorrect data set name."<<endl;
+    return 0;
+  }
+
+  string line="";
+  getline(in,line);
+  vector<string> headers=split(line);
+  unsigned int nb_examples=stoi(headers[0]);
 
   // hyper-params
-  const unsigned int num_input = 9;//9
-  const unsigned int num_output = 1;
-  const unsigned int num_layers = 2;
-  const unsigned int nb_hid_units = 2;
-  const float desired_error = (const float) 0.00f;
-  const unsigned int max_epochs = 100;
-  const unsigned int epochs_between_reports = 1;
+  const unsigned int num_input=stoi(headers[1]);
+  const unsigned int num_output=stoi(headers[2]);
+  const unsigned int num_layers=2;
+  const unsigned int nb_hid_units=20;
+  const float desired_error=(const float) 0.00f;
+  const unsigned int max_epochs=100;
+  const unsigned int epochs_between_reports=1;
 
   fann_type min_weight=-1;
   fann_type max_weight=+1;
 
-  struct fann_train_data *data = fann_read_train_from_file(ds_filename);
+  struct fann_train_data *data = fann_read_train_from_file(ds_filename.c_str());
   struct fann *ann=fann_create_standard(num_layers,
 					num_input,
 					num_output,
 					nb_hid_units);
   fann_randomize_weights(ann,min_weight,max_weight);
-  cout<<"created net:"<<endl
-      <<"nb inputs     ="<<fann_get_num_input(ann)<<endl
-      <<"total nb connections ="<<fann_get_total_connections(ann)<<endl
-      <<"nb outputs    ="<<fann_get_num_output(ann)<<endl
-      <<"nb hid. layers="<<fann_get_num_layers(ann)<<endl;
 
   cout<<"setting activation functions"<<endl;
   fann_set_activation_function_hidden(ann, FANN_SIGMOID_SYMMETRIC);
@@ -53,9 +92,16 @@ int main(){
       min=mse;
   }
   
-  cout<<"lowest err obtained after"<<nb_opt_algs<<" GD/BP runs for "<<max_epochs<<" epochs is "<<min<<endl;
+  cout<<"lowest err obtained after "<<nb_opt_algs<<" != runs of BP for "<<max_epochs<<" epochs is "<<min<<endl;
 
-  fann_save(ann, "bcm_float.net");
+  cout<<"net topology:"<<endl
+      <<"nb inputs     ="<<fann_get_num_input(ann)<<endl
+      <<"total nb connections ="<<fann_get_total_connections(ann)<<endl
+      <<"nb outputs    ="<<fann_get_num_output(ann)<<endl
+      <<"nb hid. layers="<<fann_get_num_layers(ann)<<endl;
+
+
+  //fann_save(ann, "bcm_float.net");
   fann_destroy(ann);
   return 0;
 }
