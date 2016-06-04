08dd5274b700c414ffe22b64106f3cfbadede9ea
HichameMoriceau <moriceau.hichame@gmail.com>
2016-06-04 13:37:16 +0100

    Added working implementation of diverse topology training using Back Propagation.

--- BP_experiment/main.cpp
+++ BP_experiment/main.cpp
@@ -22,8 +22,13 @@ vector<string> split(const string& str, int delimiter(int) = ::isspace){
   return result;
 }
 
+// generates random integer within range
+int rand_int(int min, int max){
+  return min + (rand() % (int)(max - min + 1));
+}
+
 double fixed_topo_exp(){
-  //const char* ds_filename="data/breast-cancer-recurrence-data-transformed.data";//"data_sets/breast-cancer-malignantOrBenign-data-transformed.data";
+  double res=-1;
   unsigned int nb_opt_algs=5;
   
   vector<char*> ds_filenames;
@@ -46,6 +51,7 @@ double fixed_topo_exp(){
   getline(in,line);
   vector<string> headers=split(line);
   unsigned int nb_examples=stoi(headers[0]);
+  in.close();
 
   // hyper-params
   const unsigned int num_input=stoi(headers[1]);
@@ -53,7 +59,7 @@ double fixed_topo_exp(){
   const unsigned int num_layers=2;
   const unsigned int nb_hid_units=20;
   const float desired_error=(const float) 0.00f;
-  const unsigned int max_epochs=100;
+  const unsigned int max_epochs=20;
   const unsigned int epochs_between_reports=1;
 
   fann_type min_weight=-1;
@@ -71,7 +77,7 @@ double fixed_topo_exp(){
   fann_set_activation_function_output(ann, FANN_SIGMOID_SYMMETRIC);
 
   cout<<"training model"<<endl;
-  double min=10;
+  res=10;
   for(unsigned int b=0;b<nb_opt_algs;b++){
     //reset ann
     fann_randomize_weights(ann,min_weight,max_weight);
@@ -81,20 +87,97 @@ double fixed_topo_exp(){
       mse=fann_train_epoch(ann,data);
       cout<<"epoch"<<i<<"\tmse="<<mse<<endl;
     }
-    if(mse<min)
-      min=mse;
+    if(mse<res)
+      res=mse;
   }
   
-  cout<<"lowest err obtained after "<<nb_opt_algs<<" != runs of BP for "<<max_epochs<<" epochs is "<<min<<endl<<endl;
+  cout<<"lowest err obtained after "<<nb_opt_algs<<" != runs of BP for "<<max_epochs<<" epochs is "<<res<<endl<<endl;
 
   cout<<"Using the same topology for all networks:"<<endl
       <<"\tNb inputs            = "<<fann_get_num_input(ann)<<endl
       <<"\tTotal nb connections = "<<fann_get_total_connections(ann)<<endl
       <<"\tNb outputs           = "<<fann_get_num_output(ann)<<endl
-      <<"\tNb hid. layers       = "<<fann_get_num_layers(ann)<<endl;
+      <<"\tNb hid. layers       = "<<fann_get_num_layers(ann)-2<<endl;
 
   //fann_save(ann, "bcm_float.net");
   fann_destroy(ann);
+  return res;
+}
+
+double diverse_topo_exp(){
+  double res=0;
+  unsigned int nb_opt_algs=5;
+  
+  vector<char*> ds_filenames;
+  ds_filenames.push_back((char*)"data/wine-data-transformed.data"); // multi-class
+  ds_filenames.push_back((char*)"data/breast-cancer-recurrence-data-transformed.data");
+  ds_filenames.push_back((char*)"data/iris-data-transformed.data"); // multi-class
+  ds_filenames.push_back((char*)"data/breast-cancer-malignantOrBenign-data-transformed.data");
+
+  // selected data set
+  string ds_filename=string(ds_filenames[0]);
+  
+  // read data set header
+  ifstream in(ds_filename);
+  if(!in.is_open()){
+    cout<<"Incorrect data set name."<<endl;
+    return 0;
+  }
+
+  string line="";
+  getline(in,line);
+  vector<string> headers=split(line);
+  unsigned int nb_examples=stoi(headers[0]);
+  in.close();
+  
+  // hyper-params
+  unsigned int num_input=stoi(headers[1]);
+  unsigned int num_output=stoi(headers[2]);
+  unsigned int num_layers=2;
+  unsigned int nb_hid_units=20;
+  const float desired_error=(const float) 0.00f;
+  const unsigned int max_epochs=10;
+  const unsigned int epochs_between_reports=1;
+
+  fann_type min_weight=-1;
+  fann_type max_weight=+1;
+
+  struct fann_train_data *data = fann_read_train_from_file(ds_filename.c_str());
+  struct fann *ann;
+
+  cout<<"training model"<<endl;
+  res=10;
+  for(unsigned int b=0;b<nb_opt_algs;b++){
+    // randomize depth and size
+    num_layers=rand_int(1,3);
+    nb_hid_units=rand_int(1,20);
+    // encode topology description in array
+    vector<unsigned int> desc_vec;
+    desc_vec.push_back(num_input);
+    for(unsigned int i=0;i<num_layers;i++)
+      desc_vec.push_back(nb_hid_units);
+    desc_vec.push_back(num_output);
+    // cast to array
+    unsigned int *desc_array=&desc_vec[0];
+    // instantiate model
+    ann=fann_create_standard_array(num_layers+2,desc_array); // total NB layers = nb hid layers + input & output layer    
+    // use sigmoid activation function for all neurons
+    fann_set_activation_function_hidden(ann, FANN_SIGMOID_SYMMETRIC);
+    fann_set_activation_function_output(ann, FANN_SIGMOID_SYMMETRIC);
+    
+    double mse=-1;
+    cout<<"running BP"<<b<<endl;
+    for(unsigned int i=0;i<max_epochs;i++){
+      mse=fann_train_epoch(ann,data);
+      cout<<"epoch"<<i<<"\tmse="<<mse<<endl;
+    }
+    // clear heap
+    fann_destroy(ann);
+    if(mse<res)
+      res=mse;
+  }
+  cout<<"lowest err obtained after "<<nb_opt_algs<<" != runs of BP for "<<max_epochs<<" epochs is "<<res<<endl<<endl;  
+  return res;
 }
 
  /**
@@ -122,21 +205,22 @@ int main(int argc, char * argv []){
 
   switch(exp_index){
   case 0:
-    ss<<"IDENTICAL_TOPOLOGY using "<<nb_reps<<" replicates\n";
-    cout<<ss.str();
+    ss<<"IDENTICAL_TOPOLOGY using "<<nb_reps<<" replicates";
+    cout<<ss.str()<<endl;
     res=fixed_topo_exp();
     break;
   case 1:
-    ss<<"DIVERSE_TOPOLOGY using "<<nb_reps<<" replicates\n";
-    cout<<ss.str();
+    ss<<"DIVERSE_TOPOLOGY using "<<nb_reps<<" replicates";
+    cout<<ss.str()<<endl;
+    res=diverse_topo_exp();
     break;
   default:
-    ss<<"IDENTICAL_TOPOLOGY using "<<nb_reps<<" replicates\n";
-    cout<<ss.str();
+    ss<<"IDENTICAL_TOPOLOGY using "<<nb_reps<<" replicates";
+    cout<<ss.str()<<endl;
     res=fixed_topo_exp();
   }
 
-  cout<<"Finished "<<ss.str();
+  cout<<"Finished "<<ss.str()<<". Best MSE="<<res<<endl;
 
   return 0;
 }
