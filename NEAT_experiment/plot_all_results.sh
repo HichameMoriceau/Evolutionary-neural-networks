0b9a40578fde1545edad37ddbb5f5c3a9bb5d10f
HichameMoriceau <moriceau.hichame@gmail.com>
2016-06-05 22:46:58 +0100

    Added quick bash script to run all experiments

--- NEAT_experiment/plot_all_results.sh
+++ NEAT_experiment/plot_all_results.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-(exec "./plotter.m" "data/wine_data_transformed_results/results.mat" "Wine")
+(exec "./plotter.m" "data/results-neat-bcm.mat" "BCM-NEAT")
 
-(exec "./plotter.m" "data/iris_data_transformed_results/results.mat" "Iris")
+(exec "./plotter.m" "data/results-neat-iris.mat" "IRIS-NEAT")
 
-(exec "./plotter.m" "data/breast_cancer_malignantOrBenign_data_transformed_results/results.mat" "BC-Malignancy")
+(exec "./plotter.m" "data/results-neat-wine.mat" "WINE-NEAT")
 
-(exec "./plotter.m" "data/breast_cancer_recurrence_data_transformed_results/results.mat" "BC-Recurrence")
+#(exec "./plotter.m" "data/results-neat-bcr.mat" "BCR-NEAT")
---
+++ NEAT_experiment/run_experiment.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./neat test.ne 5 30 150
+
+./neat test.ne 6 30 150
+
+./neat test.ne 7 30 150
+
