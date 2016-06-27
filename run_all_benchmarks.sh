1ef36aec59e217acd0c2ab8c578df8df1b76c773
HichameMoriceau <moriceau.hichame@gmail.com>
2016-06-27 10:16:25 +0100

    Fix main running script (BP experiment doesn't expect .DATA filenames any more => still expect .DATA files though).

--- run_all_benchmarks.sh
+++ run_all_benchmarks.sh
@@ -13,13 +13,12 @@
 # Please be patient! Running each benchmark can take some time! 
 DATA_SETS="data/breast-cancer-malignantOrBenign-data-transformed.csv data/breast-cancer-recurrence-data-transformed.csv data/iris-data-transformed.csv data/wine-data-transformed.csv"
 
-BP_DATA_SETS="data/breast-cancer-malignantOrBenign-data-transformed.data data/breast-cancer-recurrence-data-transformed.data data/iris-data-transformed.data data/wine-data-transformed.data"
 NB_REPS=1
 NB_ERR_FUNC_CALLS=150
 POP_SIZE=100
 
 # BP-CASCADE TOPOLOGY EXPERIMENTS
-./BP_experiment/runme $BP_DATA_SETS $NB_REPS $NB_ERR_FUNC_CALLS
+./BP_experiment/runme $DATA_SETS $NB_REPS $NB_ERR_FUNC_CALLS
 
 # Neuro Evolution of Augmenting Topologies
 ./NEAT_experiment/neat $DATA_SETS NEAT_experiment/test.ne $NB_REPS $NB_ERR_FUNC_CALLS $POP_SIZE 
